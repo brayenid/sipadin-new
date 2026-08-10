2ef74042d1a64f794db6d3b51248c8ba7925f406
Brayen Luhat <bfenomenal@gmail.com>
2026-08-10 11:53:06 +0800

    fix(build): add @react-pdf/renderer to serverExternalPackages to fix ESM import compilation error on server

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['tedious-lucca-sickly.ngrok-free.dev'],
+  serverExternalPackages: ['@react-pdf/renderer'],
 };
 
 export default nextConfig;
