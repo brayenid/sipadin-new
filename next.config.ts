c4fde61139b3a1fc833cdbb4d4ba118ecd9fb1a5
Brayen Luhat <bfenomenal@gmail.com>
2026-08-10 11:58:17 +0800

    revert: back to Turbopack dev server, remove --webpack and serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['tedious-lucca-sickly.ngrok-free.dev'],
-  serverExternalPackages: ['@react-pdf/renderer'],
 };
 
 export default nextConfig;
